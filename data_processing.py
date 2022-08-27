377dd0bebd8b0baf8cd3d628a6e35c2ff040430b
rahulkpareek <r240825@gmail.com>
2022-08-27 23:22:19 +0200

    Revert "updated data processing file"
    This reverts commit 74e2dbb8585d33f436488f2d220b7a61737eca8d.

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,2 @@
-#Dummy code to process the data
-print("Processing all the data to perform analysis")
-
-
-#Some faulty code also commited
-print('faulty code')
+#example
+print(Processing all the data to perform analysis)
