74e2dbb8585d33f436488f2d220b7a61737eca8d
rahulkpareek <r240825@gmail.com>
2022-08-27 23:21:11 +0200

    updated data processing file

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,6 @@
-#example
-print(Processing all the data to perform analysis)
+#Dummy code to process the data
+print("Processing all the data to perform analysis")
+
+
+#Some faulty code also commited
+print('faulty code')
